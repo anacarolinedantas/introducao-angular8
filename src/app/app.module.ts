1357b6b258be31a1991d057c5f83a71d96841443
Carol Dantas <caroldantas@ufrn.edu.br>
2020-11-01 11:13:44 -0300

    Segrega component Error404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Error404Component
   ],
   imports: [
     BrowserModule,
@@ -21,9 +19,6 @@ import { CoreModule } from './core/core.module';
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
       },
-      {
-        path: '**', component: Error404Component
-      }
     ])
   ],
   providers: [],

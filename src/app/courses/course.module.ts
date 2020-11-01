5b7d39cd8af6ed9aa074da8a4c147e86eb5f4a47
Carol Dantas <caroldantas@ufrn.edu.br>
2020-11-01 10:46:52 -0300

    Segrega módulo pipe

--- src/app/courses/course.module.ts
+++ src/app/courses/course.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { AppPipeModule } from '../shared/component/pipe/app-pipe.module';
+import { AppPipeModule } from '../shared/pipe/app-pipe.module';
 import { StarModule } from '../shared/component/star/star.module';
 import { CourseInfoComponent } from './course-info.component';
 import { CourseListComponent } from './course-list.component';
